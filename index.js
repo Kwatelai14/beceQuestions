6606d140ba3dd4ef867a1b8ef12962acc5783065
Kwatelai14 <stdiallo14@gmail.com>
2023-06-29 16:29:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var accordionHeaders = document.querySelectorAll(".objective-answers");
+
+for (var i = 0; i < accordionHeaders.length; i++) {
+  accordionHeaders[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var accordionContent = this.nextElementSibling;
+    if (accordionContent.style.display === "block") {
+      accordionContent.style.display = "none";
+    } else {
+      accordionContent.style.display = "block";
+    }
+  });
+}
